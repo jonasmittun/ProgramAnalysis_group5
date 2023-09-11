0a99f1dd1790372f6d7140fc804ae87ddd5b9922
Zarkino <fhv2325@gmail.com>
2023-09-11 16:51:11 +0200

    Added a small test

--- Week03/src/main/java/Week3/Main.java
+++ Week03/src/main/java/Week3/Main.java
@@ -2,8 +2,34 @@ package Week3;
 
 import org.json.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        File file = new File("src\\main\\java\\dtu\\json\\Outer.json");
 
+        try {
+            String content = new String(Files.readAllBytes(Paths.get(file.toURI())));
+            JSONObject o = new JSONObject(content);
+
+            process(o);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void process(JSONObject object) {
+        List<Object> access = object.getJSONArray("access").toList();
+        System.out.println(access);
+
+        List<Object> typeParams = object.getJSONArray("typeparams").toList();
+        System.out.println(typeParams);
+
+        String name = object.getString("name");
+        System.out.println(name);
     }
 }
