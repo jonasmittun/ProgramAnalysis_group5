16866302931744f8b41fc02a7693bfd91f01a9c3
Zarkino <fhv2325@gmail.com>
2023-09-19 16:36:11 +0200

    Changes by pabloramoshernandez

--- Week04/src/main/java/Week4/Main.java
+++ Week04/src/main/java/Week4/Main.java
@@ -4,7 +4,9 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
