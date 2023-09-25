018f45e56dc3fd7caa95861eb13bf073aaa915cb
Zarkino <fhv2325@gmail.com>
2023-09-25 19:21:08 +0200

    Split execution into single steps

--- Week04/src/main/java/Week4/Interpreter.java
+++ Week04/src/main/java/Week4/Interpreter.java
@@ -7,8 +7,16 @@ import java.util.*;
 public interface Interpreter {
 
     /** Runs the method
-     * mu:      Memory
-     * first:   Method
-     * */
-    void run(Method first, Map<Integer, JSONObject> mu);
+     * @param method:   Method
+     * @param mu:       Memory
+     */
+    void run(Method method, Map<Integer, JSONObject> mu);
+
+    /**
+     * Execute a single bytecode instruction in the method
+     * @param method:   The method to be executed
+     * @param mu:       The memory
+     * @param psi:      The method stack
+     */
+    void step(Method method, Map<Integer, JSONObject> mu, Stack<Method> psi);
 }
