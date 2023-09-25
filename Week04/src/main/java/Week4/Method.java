89b41c5667fb67fb27de1bc6eacd35e14dbc270a
Zarkino <fhv2325@gmail.com>
2023-09-25 16:20:12 +0200

    Make Method public

--- Week04/src/main/java/Week4/Method.java
+++ Week04/src/main/java/Week4/Method.java
@@ -11,7 +11,7 @@ import java.util.Stack;
  * sigma:   Operand Stack
  * iota:    Program Counter
  */
-record Method(JSONObject[] lambda, Stack<JSONObject> sigma, Pair<String, Integer> iota) {
+public record Method(JSONObject[] lambda, Stack<JSONObject> sigma, Pair<String, Integer> iota) {
     @Override
     public String toString() {
         return "(λ" + Arrays.toString(lambda) + ", σ" + sigma + ", ι" + iota.toString() + ")";
