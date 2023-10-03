f5daef856baaf7eaf33b315c8594cddebc4f4f17
peterjahola <jahola.peter@gmail.com>
2023-10-03 13:43:45 +0200

    Created week06 package with previous week data

---
+++ src/main/java/Week06/State.java
@@ -0,0 +1,20 @@
+package Week06;
+
+import Week04.Method;
+import org.json.JSONObject;
+
+import java.util.AbstractMap;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public record State(Deque<Method> psi, Map<Integer, JSONObject> mu) {
+    @Override
+    public State clone() {
+        Deque<Method> psi = this.psi.stream().map(Method::clone).collect(Collectors.toCollection(ArrayDeque::new));
+        Map<Integer, JSONObject> mu = this.mu.entrySet().stream().map(e -> new AbstractMap.SimpleEntry<>(e.getKey(), new JSONObject(e.getValue().toMap()))).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        return new State(psi, mu);
+    }
+}
---
+++ src/test/java/Week06/SimpleTest.java
@@ -0,0 +1,206 @@
+package Week06;
+
+import Week04.Main;
+import Week04.Method;
+import Week04.Pair;
+import Week05.SignInterpreter;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static Week01.Main.getFiles;
+import static Week05.Sign.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SimpleTest {
+
+    static Map<String, String> mapper = new HashMap<>();       // Map<Filename, Classname>
+    static Map<String, JSONObject> classes = new HashMap<>();  // Map<Classname, JSONObject>
+
+    static JSONObject cls;
+
+    static int depthLimit = 20;
+
+    @BeforeAll
+    static void initAll() {
+        String path = "src\\main\\resources\\decompiled";
+        for(Map.Entry<String, String> entry : getFiles(path).entrySet()) {
+            String filename = entry.getKey();
+            String content = entry.getValue();
+
+            JSONObject file = new JSONObject(content);
+            String classname = file.getString("name");
+
+            classes.put(classname, file);
+            mapper.put(filename, classname);
+        }
+
+        cls = classes.get(mapper.get("Simple.json"));
+    }
+
+    @Test
+    void noop() {
+        Method m = new Method(new JSONObject[] {}, new ArrayDeque<>(), new Pair<>(Main.simpleResolve(cls, "noop"), 0));
+        Map<Integer, JSONObject> mu = new HashMap<>();
+
+        Week05.SignInterpreter in = new Week05.SignInterpreter(new HashMap<>(classes), depthLimit);
+        in.run(m, mu);
+    }
+
+    @Test
+    void zero() {
+        Method m = new Method(new JSONObject[] {}, new ArrayDeque<>(), new Pair<>(Main.simpleResolve(cls, "zero"), 0));
+        Map<Integer, JSONObject> mu = new HashMap<>();
+
+        Week05.SignInterpreter in = new Week05.SignInterpreter(new HashMap<>(classes), depthLimit);
+        in.run(m, mu);
+    }
+
+    @Test
+    void hundredAndTwo() {
+        Method m = new Method(new JSONObject[] {}, new ArrayDeque<>(), new Pair<>(Main.simpleResolve(cls, "hundredAndTwo"), 0));
+        Map<Integer, JSONObject> mu = new HashMap<>();
+
+        Week05.SignInterpreter in = new Week05.SignInterpreter(new HashMap<>(classes), depthLimit);
+        in.run(m, mu);
+    }
+
+    @Test
+    void identity() {
+        Method m = new Method(new JSONObject[] { new JSONObject(Map.of("type", "int", "value", 7)) }, new ArrayDeque<>(), new Pair<>(Main.simpleResolve(cls, "identity"), 0));
+        Map<Integer, JSONObject> mu = new HashMap<>();
+
+        Week05.SignInterpreter in = new Week05.SignInterpreter(new HashMap<>(classes), depthLimit);
+        in.run(m, mu);
+    }
+
+    @Nested
+    @DisplayName("add tests")
+    class Add {
+        @Test
+        void add_int_input() {
+            Method m = new Method(new JSONObject[] { new JSONObject(Map.of("type", "int", "value", -1)), new JSONObject(Map.of("type", "int", "value", 2)) }, new ArrayDeque<>(), new Pair<>(Main.simpleResolve(cls, "add"), 0));
+            Map<Integer, JSONObject> mu = new HashMap<>();
+
+            Week05.SignInterpreter in = new Week05.SignInterpreter(new HashMap<>(classes), depthLimit);
+            in.run(m, mu);
+        }
+        @Test
+        void add_full_sets_input() {
+            Method m = new Method(new JSONObject[] { new JSONObject(Map.of("type", "int", "value", 1, "sign", new JSONArray(Set.of(NEGATIVE, ZERO, POSITIVE)))), new JSONObject(Map.of("type", "int", "value", 2, "sign", new JSONArray(Set.of(NEGATIVE, ZERO, POSITIVE)))) }, new ArrayDeque<>(), new Pair<>(Main.simpleResolve(cls, "add"), 0));
+            Map<Integer, JSONObject> mu = new HashMap<>();
+
+            Week05.SignInterpreter in = new Week05.SignInterpreter(new HashMap<>(classes), depthLimit);
+            in.run(m, mu);
+        }
+    }
+
+
+    @Nested
+    @DisplayName("min tests")
+    class Min {
+        @Test
+        void min_int_input() {
+            Method m = new Method(new JSONObject[] { new JSONObject(Map.of("type", "int", "value", 1)), new JSONObject(Map.of("type", "int", "value", 2)) }, new ArrayDeque<>(), new Pair<>(Main.simpleResolve(cls, "min"), 0));
+            Map<Integer, JSONObject> mu = new HashMap<>();
+
+            Week05.SignInterpreter in = new Week05.SignInterpreter(new HashMap<>(classes), depthLimit);
+            in.run(m, mu);
+        }
+        @Test
+        void min_full_sets_input() {
+            Method m = new Method(new JSONObject[] { new JSONObject(Map.of("type", "int", "value", 1, "sign", new JSONArray(Set.of(NEGATIVE, ZERO, POSITIVE)))), new JSONObject(Map.of("type", "int", "value", 2, "sign", new JSONArray(Set.of(NEGATIVE, ZERO, POSITIVE)))) }, new ArrayDeque<>(), new Pair<>(Main.simpleResolve(cls, "min"), 0));
+            Map<Integer, JSONObject> mu = new HashMap<>();
+
+            Week05.SignInterpreter in = new Week05.SignInterpreter(new HashMap<>(classes), depthLimit);
+            in.run(m, mu);
+        }
+    }
+
+    @Nested
+    @DisplayName("div tests")
+    class div {
+        @Test
+        void div_int_input() {
+            Method m = new Method(new JSONObject[] { new JSONObject(Map.of("type", "int", "value", 4)), new JSONObject(Map.of("type", "int", "value", 2)) }, new ArrayDeque<>(), new Pair<>(Main.simpleResolve(cls, "div"), 0));
+            Map<Integer, JSONObject> mu = new HashMap<>();
+
+            Week05.SignInterpreter in = new Week05.SignInterpreter(new HashMap<>(classes), depthLimit);
+            in.run(m, mu);
+        }
+
+        @Test
+        void div_int_input_fail() {
+            Method m = new Method(new JSONObject[] { new JSONObject(Map.of("type", "int", "value", 4)), new JSONObject(Map.of("type", "int", "value", 0)) }, new ArrayDeque<>(), new Pair<>(Main.simpleResolve(cls, "div"), 0));
+            Map<Integer, JSONObject> mu = new HashMap<>();
+
+            Week05.SignInterpreter in = new Week05.SignInterpreter(new HashMap<>(classes), depthLimit);
+            Exception exception = assertThrows(ArithmeticException.class, () -> in.run(m, mu));
+
+            assertTrue(exception.getMessage().contains("Illegal divide by zero"));
+        }
+
+        @Test
+        void div_sets_input() {
+            Method m = new Method(new JSONObject[] { new JSONObject(Map.of("type", "int", "value", 1, "sign", new JSONArray(Set.of(NEGATIVE, ZERO, POSITIVE)))), new JSONObject(Map.of("type", "int", "value", 2, "sign", new JSONArray(Set.of(NEGATIVE, POSITIVE)))) }, new ArrayDeque<>(), new Pair<>(Main.simpleResolve(cls, "div"), 0));
+            Map<Integer, JSONObject> mu = new HashMap<>();
+
+            Week05.SignInterpreter in = new Week05.SignInterpreter(new HashMap<>(classes), depthLimit);
+            in.run(m, mu);
+        }
+
+        @Test
+        void div_sets_input_fail() {
+            Method m = new Method(new JSONObject[] { new JSONObject(Map.of("type", "int", "value", 1, "sign", new JSONArray(Set.of(NEGATIVE, ZERO, POSITIVE)))), new JSONObject(Map.of("type", "int", "value", 0, "sign", new JSONArray(Set.of(ZERO)))) }, new ArrayDeque<>(), new Pair<>(Main.simpleResolve(cls, "div"), 0));
+            Map<Integer, JSONObject> mu = new HashMap<>();
+
+            Week05.SignInterpreter in = new Week05.SignInterpreter(new HashMap<>(classes), depthLimit);
+            Exception exception = assertThrows(ArithmeticException.class, () -> in.run(m, mu));
+
+            assertTrue(exception.getMessage().contains("Illegal divide by zero"));
+        }
+
+        @Test
+        void div_full_sets_input_fail() {
+            Method m = new Method(new JSONObject[] { new JSONObject(Map.of("type", "int", "value", 1, "sign", new JSONArray(Set.of(NEGATIVE, ZERO, POSITIVE)))), new JSONObject(Map.of("type", "int", "value", 0, "sign", new JSONArray(Set.of(NEGATIVE, ZERO, POSITIVE)))) }, new ArrayDeque<>(), new Pair<>(Main.simpleResolve(cls, "div"), 0));
+            Map<Integer, JSONObject> mu = new HashMap<>();
+
+            Week05.SignInterpreter in = new Week05.SignInterpreter(new HashMap<>(classes), depthLimit);
+            Exception exception = assertThrows(ArithmeticException.class, () -> in.run(m, mu));
+
+            assertTrue(exception.getMessage().contains("Illegal divide by zero"));
+        }
+    }
+
+
+    @Nested
+    @DisplayName("factorial tests")
+    class Factorial {
+        @Test
+        void factorial_int() {
+            Method m = new Method(new JSONObject[] { new JSONObject(Map.of("type", "int", "value", 6)), new JSONObject(Map.of("type", "int", "value", 0)) }, new ArrayDeque<>(), new Pair<>(Main.simpleResolve(cls, "factorial"), 0));
+            Map<Integer, JSONObject> mu = new HashMap<>();
+
+            Week05.SignInterpreter in = new Week05.SignInterpreter(new HashMap<>(classes), depthLimit);
+            in.run(m, mu);
+        }
+
+        @Test
+        void factorial_sets() {
+            Method m = new Method(new JSONObject[] { new JSONObject(Map.of("type", "int", "value", 6, "sign", new JSONArray(Set.of(NEGATIVE, ZERO, POSITIVE)))), new JSONObject(Map.of("type", "int", "value", 0, "sign", new JSONArray(Set.of(NEGATIVE, ZERO, POSITIVE)))) }, new ArrayDeque<>(), new Pair<>(Main.simpleResolve(cls, "factorial"), 0));
+            Map<Integer, JSONObject> mu = new HashMap<>();
+
+            Week05.SignInterpreter in = new SignInterpreter(new HashMap<>(classes), depthLimit);
+            in.run(m, mu);
+        }
+    }
+}
