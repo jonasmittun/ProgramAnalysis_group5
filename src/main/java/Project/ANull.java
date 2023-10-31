e3b9e8cc71ac4d607d6b9be12498ef9fa182b543
Zarkino <fhv2325@gmail.com>
2023-10-31 16:31:33 +0100

    Implemented values for Anull Enum

--- src/main/java/Project/ANull.java
+++ src/main/java/Project/ANull.java
@@ -1,5 +1,39 @@
 package Project;
 
-public enum ANull implements Abstraction {
-    // TODO: Implement values and methods
+import org.json.JSONObject;
+
+import java.util.Map;
+
+public enum ANull {
+    NULL,
+    NULLABLE,
+    NOTNULL;
+
+    /** Translates JSONObject o to an abstract type
+     * @param o JSONObject
+     */
+    public static JSONObject toAbstract(JSONObject o) {
+        if(o == null || o.isEmpty()) {
+            o = new JSONObject(Map.of("abstract", NULL));
+        } else {
+            if(!o.has("abstract")) {
+                if(o.has("type")) { // BaseType
+                    o.put("abstract", NOTNULL);
+                } else if (o.has("kind")) { // SimpleReferenceType
+                    o.put("abstract", NULLABLE);
+                } else throw new RuntimeException(o + " is not a SimpleType!");
+            }
+        }
+
+        return o;
+    }
+
+    @Override
+    public String toString() {
+        return switch(this) {
+            case NULL       -> "Null";
+            case NULLABLE   -> "Nullable";
+            case NOTNULL    -> "Not Null";
+        };
+    }
 }
